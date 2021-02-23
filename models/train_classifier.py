42bca21036be07b1027b18b0addec6e1b7965928
jochenruland <jochenhgruland@gmail.com>
2021-02-23 10:13:08 +0100

    Setup of ML pipeline with function to load data

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,12 +1,56 @@
+# import libraries
 import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+
+import re
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger', 'stopwords'])
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+
+
 
 
 def load_data(database_filepath):
-    pass
+    ''' Function that loads dataset from a database into a Pandas Dataframe 
+        and splits the data into an Input Dataset and an Output Dataset
+
+        Arguments:
+            database_filepath - filepath
+
+        Returns:
+            X - pandas Series
+            Y - pandas Dataframe
+    '''
+    engine = create_engine('sqlite:///Desaster_messages_categories.db')
+    df = pd.read_sql('SELECT * FROM Desaster_messages_categories', engine)
+
+    X = df['message'] # define input data
+    Y = df.iloc[:,3:39] # define output data
+    print(Y.head(1), Y.shape)
+    print(X.head(1), X.shape)
+
+    return X, Y
+
 
 
 def tokenize(text):
     pass
+
 
 
 def build_model():
@@ -27,13 +71,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
